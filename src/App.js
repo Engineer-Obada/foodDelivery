3b1f722dcce45c567566e59983220a332ab95c43
Engineer-Obada <obadaalsayed1999.com>
2022-11-08 16:58:25 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import './App.css';
-import Home from './Pages/Home/Home'
-import {BrowserRouter as Router,
-        Routes,
-        Route,
-        Link
-} from 'react-router-dom'
-import Favorite from './Pages/Favoirt/Favorite';
-import Layout from './Component/Layout/Layout';
-import AddProduct from './Pages/AddProduct/AddProduct';
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Favorite from "./Pages/Favoirt/Favorite";
+import Layout from "./Component/Layout/Layout";
+import AddProduct from "./Pages/AddProduct/AddProduct";
 
-function App () {
-
+function App() {
   return (
     <Router>
-      <div className='App'>
-      <Layout /> 
+      <div className="App">
+        <Layout />
         <Routes>
-        <Route exact path='/' element={<Home />}></Route>
-        <Route exact  path='/Favorit' element={<Favorite />}></Route>
-        <Route exact path='/Addcategory' element={<AddProduct />}></Route>
+          <Route exact path="/foodDelivery" element={<Home />}></Route>
+          <Route exact path="/Favorit" element={<Favorite />}></Route>
+          <Route exact path="/Addcategory" element={<AddProduct />}></Route>
         </Routes>
       </div>
     </Router>
-
-  
-
   );
 }
 
